b4c958cc4d404ad9ad6c12ea4a18da609a0cc8b5
KallaneKoe <namnguyenhoang0903@gmail.com>
2025-01-05 21:36:16 +0700

    commit

--- middleware/manager-only.js
+++ middleware/manager-only.js
@@ -1,10 +1,10 @@
-export default defineNuxtRouteMiddleware((to) => {
-  if (process.client) {
-    const user = JSON.parse(localStorage.getItem("user") || "{}");
-    if (user.role !== "manager") {
-      return abortNavigation("/admin");
-    }
-  } else {
-    return abortNavigation("/admin");
-  }
-});
+// export default defineNuxtRouteMiddleware((to) => {
+//   if (process.client) {
+//     const user = JSON.parse(localStorage.getItem("user") || "{}");
+//     if (user.role !== "manager") {
+//       return abortNavigation("/admin");
+//     }
+//   } else {
+//     return abortNavigation("/admin");
+//   }
+// });
